1a57546783ffc5a132f947e0c5e91ae8a57a1cf7
rmjuarez12 <rmjuarez12@gmail.com>
2020-10-02 22:13:33 -0400

    Setup state and changes for form

--- team-builder/src/components/user/UserForm.js
+++ team-builder/src/components/user/UserForm.js
@@ -1,22 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function UserForm() {
+  const [user, setUser] = useState(null);
+
+  const handleChange = (e) => {
+    const newUser = { ...user, [e.target.name]: e.target.value };
+    setUser(newUser);
+
+    console.log(newUser);
+  };
+
   return (
     <div>
       <form>
         <p>
           <label htmlFor="user-name">Name</label>
-          <input type="text" name="user-name" id="user-name" placeholder="Enter Name" />
+          <input type="text" name="name" id="user-name" placeholder="Enter Name" onChange={handleChange} />
         </p>
 
         <p>
           <label htmlFor="user-email">Email</label>
-          <input type="email" name="user-email" id="user-email" placeholder="Enter Email" />
+          <input
+            type="email"
+            name="email"
+            id="user-email"
+            placeholder="Enter Email"
+            onChange={handleChange}
+          />
         </p>
 
         <p>
           <label htmlFor="user-role">Role</label>
-          <select name="user-role" id="user-role">
+          <select name="role" id="user-role" onChange={handleChange}>
             <option value="0">Choose one</option>
             <option value="Project Manager">Project Manager</option>
             <option value="Backend Engineer">Backend Engineer</option>
--- team-builder/src/components/user/UserList.js
+++ team-builder/src/components/user/UserList.js
@@ -1,5 +1,19 @@
 import React from "react";
 
-export default function UserList() {
-  return <div>Test</div>;
+export default function UserList(props) {
+  const { users } = props;
+
+  return (
+    <div>
+      {users.map((user) => {
+        return (
+          <div className="user-card">
+            <div className="name">{user.name}</div>
+            <div className="email">{user.email}</div>
+            <div className="role">{user.role}</div>
+          </div>
+        );
+      })}
+    </div>
+  );
 }
